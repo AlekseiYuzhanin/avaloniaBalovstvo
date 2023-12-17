e552bdcfb512baecf49529d1f1e77bc5e33a1b5c
AlekseiYuzhanin <Zombehalk@gmail.com>
2023-12-18 01:35:41 +0700

    commit

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -3,9 +3,16 @@ using Microsoft.EntityFrameworkCore;
 public class ApplicationContext : DbContext
 {
     public ApplicationContext() => Database.EnsureCreated();
- 
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=qweasd123");
     }
+    public DbSet<Role> Roles {get;set;} = null!;
+    public DbSet<User> Users {get;set;} = null!;
+    public DbSet<Shift> Shift {get;set;} = null!;
+    public DbSet<ShiftAssignment> ShiftAssignments {get;set;} = null!;
+    public DbSet<Order> Orders {get;set;} = null!;
+    public DbSet<Dish> Dishes {get;set;} = null!;
+    public DbSet<OrderDish> OrderDishes {get;set;} = null!;
 }
