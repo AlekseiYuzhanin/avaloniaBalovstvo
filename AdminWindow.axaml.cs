3354d47a8872f7cf7d3792f8280a96a38fbd2b0f
AlekseiYuzhanin <Zombehalk@gmail.com>
2023-12-19 23:47:43 +0700

    commit

--- AdminWindow.axaml.cs
+++ AdminWindow.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -22,37 +23,16 @@ public partial class AdminWindow : Window
         AvaloniaXamlLoader.Load(this);   
     }
 
-    [System.Obsolete]
-    private void OnDragOver(object sender, DragEventArgs e)
-    {
-        if (e.Data.Contains(DataFormats.FileNames))
-        {
-            e.DragEffects = DragDropEffects.Copy;
-            e.Handled = true;
-        }
+    private async void OpenFile_Click(object sender, RoutedEventArgs e)
+    {       
+            Image? image = this.FindControl<Image>("UserImage");
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Directory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string[] result = await dialog.ShowAsync(this);
+            image.Source = new Avalonia.Media.Imaging.Bitmap(result[0]);
     }
-
-    private void OnDrop(object sender, DragEventArgs e)
-    {
-        Image _draggedImage = this.Find<Image>("draggedImage");
-        if (e.Data.Contains(DataFormats.FileNames))
-        {
-            var fileNames = e.Data.GetFileNames();
-
-            if (fileNames.Count() > 0)
-            {
-                var file = fileNames.First();
-
-                using (var stream = File.OpenRead(file))
-                {
-                    var bitmap = new Bitmap(stream);
-                    _draggedImage.Source = bitmap;
-                }
-            }
-
-            e.Handled = true;
-        }
-    }
+    
 }
 
 
